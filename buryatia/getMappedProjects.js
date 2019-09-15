fa10e088d9d69e48c66173b4d451290a29d192b9
Artem Beresnev <artembert@gmail.com>
2019-09-15 17:32:00 +0300

    get data

---
+++ buryatia/getMappedProjects.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const projects = require("./projects");
+
+const COST_DIVIDER = 1000000;
+const LOCATION_PREFIX = "Республика Бурятия, ";
+
+const commonArray = [].concat(...projects.map(item => item.features));
+
+const mappedProjects = commonArray
+	.map(item => item.attributes)
+	.map(item => ({
+		name: item.upname,
+		description: item.manager,
+		municipal: getLocation(item.resume),
+		type: item.status,
+		cost: item.cost,
+	}));
+
+console.log("Length:" + mappedProjects.length);
+console.log(mappedProjects[2]);
+fs.writeFile("buryatia-projects.json", JSON.stringify(mappedProjects, null, 4), () => (console.log("successful")));
+
+function getCost(cost) {
+	return Math.floor(cost / COST_DIVIDER);
+}
+
+function getLocation(address) {
+	if (address && address.includes(LOCATION_PREFIX)) {
+		return address.substring(LOCATION_PREFIX.length - 1).trim();
+	}
+	return address;
+}
